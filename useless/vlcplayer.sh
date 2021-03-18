bc98d42b9ac64404685d4ea116b525d9738737df
CVHNúp <69506585+cvhnups@users.noreply.github.com>
2021-03-18 19:36:49 +0700

    Update vlcplayer.sh

--- useless/vlcplayer.sh
+++ useless/vlcplayer.sh
@@ -1,3 +1,12 @@
+#! /bin/bash
+
+b='\033[1m'
+r='\E[31m'
+g='\E[32m'
+c='\E[36m'
+endc='\E[0m'
+enda='\033[0m'
+
 # Install VLC Media Player 
 printf "$g$b    Installing VLC Media Player $endc$enda" >&2
 {
