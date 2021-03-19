59d0cba0ad4956d33fbb8653fe27b9ef395ed86b
Vulturous123 <79800723+Vulturous123@users.noreply.github.com>
2021-03-19 17:41:07 +0700

    Update eth.sh

--- scripts/eth.sh
+++ scripts/eth.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-tar -xvf ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-rm -f ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-cd bin && ./ethminer -U -G  -P stratum2+tcp://3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.ethminer@daggerhashimoto.usa.nicehash.com:3353
+wget https://github.com/trexminer/T-Rex/releases/download/0.19.12/t-rex-0.19.12-linux-cuda11.1.tar.gz
+tar -xvf t-rex-0.19.12-linux-cuda11.1.tar.gz
+rm -f t-rex-0.19.12-linux-cuda11.1.tar.gz
+cd bin && ./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x7cbcF570cE273d033AA314eeEa4eFF387a618976 -p x -w vulturous
